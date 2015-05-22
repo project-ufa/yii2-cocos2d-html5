230136793d302dbd7dfceae16f128cd8e36a1fce
juanc <juanc@localhost.localdomain>
2015-05-21 21:28:05 -0500

    added getPublishedDir method

--- Cocos2dHtml5Asset.php
+++ Cocos2dHtml5Asset.php
@@ -3,18 +3,28 @@
 namespace projectufa\cocos2dhtml5;
 
 use yii\web\AssetBundle;
-use yii\base\View;
+use yii;
+use yii\helpers\FileHelper;
+use yii\helpers\Json;
+
 
 class Cocos2dHtml5Asset extends AssetBundle
 {
 
     public $sourcePath = '@bower/cocos2d-html5';
 
+    public static $staticSourcePath = '@bower/cocos2d-html5';
+
     public $js = [
         'CCBoot.js',
     ];
 
-    public $jsOptions = [
-        'position' => View::POS_END,
+    public $depends = [
+        'yii\bootstrap\BootstrapAsset',
     ];
+
+    public static function getPublishedDir()
+    {
+        return static::$staticSourcePath;
+    }
 }
